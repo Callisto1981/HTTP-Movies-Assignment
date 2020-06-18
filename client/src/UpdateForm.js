7fc735e7b9afbd2a4788a23129fe2d005fa83f34
Wayne <wabbruscatoorr@gmail.com>
2020-06-18 15:40:12 -0400

    update updateForm

--- client/src/UpdateForm.js
+++ client/src/UpdateForm.js
@@ -10,13 +10,72 @@ export const UpdateForm = () => {
     stars: []
   });
 
-  useEffect(() => {});
-  handleChange = () => {};
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/movies$props.match.params.id`)
+      .then(res => {
+        //console.log(res.data)
+        setMovieEdit(res.data);
+      });
+  }, [props.match.params.id]);
+
+  handleChange = e => {
+    setMovieEdit({ ...movieEdit, [e.target.name]: e.target.value });
+  };
+
+  handleSumbit = e => {
+    e.preventDefault();
+    axios
+      .put(
+        `http://localhost:5000/api/movies/${props.match.params.id}`,
+        movieEdit
+      )
+      .then(res => {
+        //console.log(res)
+        setMovieEdit({
+          id: "",
+          title: "",
+          directore: "",
+          metascore: "",
+          stars: []
+        });
+        props.setSavedList([...props.savedList, res]);
+        props.history.push("/");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   return (
     <>
       <div>
-        <form>
-          <input type="text" onChange={handleChange} value={e.target.value} />
+        <h1>Edit Movie Info</h1>
+        <form onSubmit={handleSumbit}>
+          <label htmlFor="title">Title</label>
+          <input
+            id="title"
+            name="title"
+            type="text"
+            onChange={handleChange}
+            value={movieEdit.title}
+          />
+          <label htmlFor="metascore"> Metascore</label>
+          <input
+            id="metascore"
+            name="starts"
+            type="text"
+            onChange={handleSumbit}
+            value={movieEdit.metascore}
+          />
+          <label htmlFor="stars">Stars</label>
+          <input
+            id="stars"
+            type="text"
+            name="stars"
+            placeholder="Enter Stars"
+            onChange={handleChange}
+            value={movieEdit.stars}
+          />
           <button>SUBMIT</button>
         </form>
       </div>
